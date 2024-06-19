2f194746426ce2b22e9229210e9f898d7fa453de
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2024-06-19 23:17:27 +0200

    Support MacOS

--- diet-mac-and-cheese/benchmark/bench_all.sh
+++ diet-mac-and-cheese/benchmark/bench_all.sh
@@ -2,59 +2,90 @@
 
 set -e
 
-PORT=7876
+PORT=7703
 
-for DELAY in 0 10 100; do
-    # set simulated network delay using traffic control:
-    echo "Run benchmarks with delay: ${DELAY}ms"
+for BW in 100 1000; do
+	for DELAY in 10; do
+	    # set simulated network delay using traffic control:
+	    echo "Run benchmarks with delay: ${DELAY}ms"
+		
+	    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		sudo tc qdisc del dev lo root || true
+		sudo tc qdisc add dev lo root handle 1:0 netem delay ${DELAY}msec
 
-    sudo tc qdisc del dev lo root || true
-    sudo tc qdisc add dev lo root handle 1:0 netem delay ${DELAY}msec
-    ping -c 1 127.0.0.1
+	    elif [[ "$OSTYPE" == "darwin"* ]]; then
+		# MacOS
+		echo "Configure enumlated network conditions on MacOS"
 
-    # simulate network conditions for particular port (source or destination)
-    # sudo tc qdisc del dev lo root || true
-    # sudo tc qdisc add dev lo handle 1: root htb
-    # sudo tc class add dev lo parent 1: classid 1:1 htb rate 1gbit
-    # sudo tc qdisc add dev lo parent 1:1 handle 10: netem delay ${DELAY}msec
-    # sudo tc filter add dev lo protocol ip parent 1: prio 1 u32 match ip dport $PORT 0xffff flowid 1:1
-    # sudo tc filter add dev lo protocol ip parent 1: prio 1 u32 match ip sport $PORT 0xffff flowid 1:1
+		# cleanup
+		sudo pfctl -E || true
+		sudo dnctl -q flush || true
+		sudo pfctl -f /etc/pf.conf || true
+		killall iperf3-darwin || true
 
-    # run each benchmark with given delay
-    for d in benchmarks/* ; do
-        echo "Running test: $d"
+		# sanity check: run a network test
+		echo "Before limits"
+		iperf3-darwin -s -p $PORT >/dev/null || true &
+		iperf3-darwin -c 127.0.0.1 -p $PORT -t 5
+		killall iperf3-darwin
 
-        # start prover
-        RUSTFLAGS='-C target-cpu=native' cargo run --release --bin dietmc_0p -- \
-            --text \
-            --lpn medium \
-            --relation $d/relation* \
-            --instance $d/public* \
-            --connection-addr 127.0.0.1:${PORT} \
-            prover \
-            --witness $d/private* 2> $d/${DELAY}_prover.out &
+		# define what `pipe 1` should do to traffic
+		echo "Apply limit"
+		(cat /etc/pf.conf && echo "dummynet-anchor \"customRule\"" && echo "anchor \"customRule\"") | sudo pfctl -f -
+		echo "dummynet in quick proto {udp,tcp,icmp} from any to any pipe 1" | sudo pfctl -a customRule -f -
+		sudo dnctl pipe 1 config delay $DELAY bw ${BW}Mbit/s
 
-        pid_prv=$!
+		# sanity check: run a network test
+		echo "After limits"
+		iperf3-darwin -s -p $PORT >/dev/null || true &
+		iperf3-darwin -c 127.0.0.1 -p $PORT -t 5
+		killall iperf3-darwin
+	    fi
 
-        # start verifier
-        RUSTFLAGS='-C target-cpu=native' cargo run --release --bin dietmc_0p -- \
-            --text \
-            --lpn medium \
-            --relation $d/relation* \
-            --instance $d/public* \
-            --connection-addr 127.0.0.1:${PORT} 2> $d/${DELAY}_verifier.out &
-        pid_vrf=$!
+	    ping -c 1 127.0.0.1
 
-        # wait for both to terminate
-        wait $pid_prv
-        wait $pid_vrf
+	    # run each benchmark with given network configuration
+	    for d in benchmarks/* ; do
+		echo "Running test: $d"
 
-        # print times
-        grep "time circ exec:" $d/${DELAY}_prover.out
-        grep "time circ exec:" $d/${DELAY}_verifier.out
-    done
+		# start prover
+		RUSTFLAGS='-C target-cpu=native' cargo run --release --bin dietmc_0p -- \
+		    --text \
+		    --lpn medium \
+		    --relation $d/relation* \
+		    --instance $d/public* \
+		    --connection-addr 127.0.0.1:${PORT} \
+		    prover \
+		    --witness $d/private* 2> $d/${DELAY}_${BW}_prover.out &
 
-    sudo tc qdisc del dev lo root || true
+		pid_prv=$!
+
+		# start verifier
+		RUSTFLAGS='-C target-cpu=native' cargo run --release --bin dietmc_0p -- \
+		    --text \
+		    --lpn medium \
+		    --relation $d/relation* \
+		    --instance $d/public* \
+		    --connection-addr 127.0.0.1:${PORT} 2> $d/${DELAY}_${BW}_verifier.out &
+		pid_vrf=$!
+
+		# wait for both to terminate
+		wait $pid_prv
+		wait $pid_vrf
+
+		# print times
+		grep "time circ exec:" $d/${DELAY}_prover.out
+		grep "time circ exec:" $d/${DELAY}_verifier.out
+	    done
+
+	    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		sudo tc qdisc del dev lo root || true
+	    elif [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "Reset network conditions"
+		sudo dnctl -q flush
+		sudo pfctl -f /etc/pf.conf
+	    fi
+	done
 done
 
 echo "Benchmark complete."
