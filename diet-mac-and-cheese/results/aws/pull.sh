52fb5b354a55d20e0fe78e30ccf16014926e3c06
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2024-07-22 16:07:19 +0200

    Fix directory structure for AWS benchmarks

---
+++ diet-mac-and-cheese/results/aws/pull.sh
@@ -0,0 +1,3 @@
+rsync -ravz ubuntu@3.16.129.209:~/dora/diet-mac-and-cheese/benchmark-circ-f61-rev .
+rsync -ravz ubuntu@3.16.129.209:~/dora/diet-mac-and-cheese/benchmark-ram-f61-rev .
+rsync -ravz ubuntu@3.16.129.209:~/dora/diet-mac-and-cheese/benchmark-circ-f128-rev .
